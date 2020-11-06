83d8c681aa0b538aa9d6487a87853e6432b3d7d4
Bronwyn Carr <bronwynacarr@gmail.com>
2020-11-06 12:56:04 +1100

    created basic card for index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  title = Faker::Job.title
+  description = Faker::Lorem.sentence(word_count: 20)
+  hours = Faker::Number.between(from: 1, to: 10)
+  date = Faker::Date.forward(days: 365)
+  Task.create title: title, description: description, hours: hours, date: date
+end
