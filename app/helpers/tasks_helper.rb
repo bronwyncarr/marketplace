90b589f145a7b2b5aac4de1aab035ff0bdb37ed4
bronwyncarr <bronwynacarr@gmail.com>
2020-11-12 14:44:16 +1100

    fixes picture logic for index + show

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,15 +1,9 @@
 module TasksHelper
 
   def image_for(task)
-    # if task.image.present?
-    #   task.image
-    # else
-    #   'food1.jpg'
-    # end
-    case task
-    when task.image.present?
+    if task.image.present?
       task.image
-    when task.charity.image.present?
+    elsif task.charity.image.present?
       task.charity.image
     else
       'food1.jpg'
