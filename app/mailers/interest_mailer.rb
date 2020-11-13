72f8631792adbeb960de2c57d7821de230ee2068
bronwyncarr <bronwynacarr@gmail.com>
2020-11-14 10:18:51 +1100

    heroku mail debugging

--- app/mailers/interest_mailer.rb
+++ app/mailers/interest_mailer.rb
@@ -1,8 +1,9 @@
 class InterestMailer < ApplicationMailer
   default from: 'cam022019@coderacademy.edu.au'
 
-  def send_interest_email(user)
+  def send_interest_task(user)
     @user = user
-    mail(to: @user.email, subject: 'Thanks for signing up to make a difference!')
+    mail(to: @user.email,
+      subject: 'Thanks for signing up to make a difference!')
   end
 end
