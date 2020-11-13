efebc50eb7abdaaefb259bc0db29cf3246ccda86
bronwyncarr <bronwynacarr@gmail.com>
2020-11-13 19:26:44 +1100

    adds mailer for volunteer

---
+++ app/mailers/interest_mailer.rb
@@ -0,0 +1,9 @@
+class InterestMailer < ApplicationMailer
+  default from: 'cam022019@coderacademy.edu.au'
+
+  def send_interest_email(user, task)
+    @user = user
+    @task = task
+    mail( :to => @user.email,:subject => 'Thanks for signing up to make a difference!' )
+  end
+end
