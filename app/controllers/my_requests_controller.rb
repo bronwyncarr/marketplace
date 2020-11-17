d7ae94ad04887aa1e9224b3bf27c53b3cbe1416f
bronwyncarr <bronwynacarr@gmail.com>
2020-11-17 20:13:17 +1100

    logic for charity index and who can create charity

--- app/controllers/my_requests_controller.rb
+++ app/controllers/my_requests_controller.rb
@@ -1,18 +1,31 @@
 class MyRequestsController < ApplicationController
   before_action :set_task, only: :show
 
+  # Show tasks from all charities that the user is associated with EVEN if they didn't create them
   def index
-     # Show requests made by the current user
-    @new_tasks = Task.current.where(user_id: current_user).includes([:charity, image_attachment: :blob])
-    @old_tasks = Task.previous.where(user_id: current_user).includes([:charity, image_attachment: :blob])
+    # Find all the charities the user is associated with through current_user.charities
+    # This may returns multiple rows of charities
+    # Then for each charity in the array look for all Tasks where the charity_id of the task has that charities.id
+    # Must use .where as multiple tasks could have the same charity id
+    @new_tasks = []
+    @old_tasks = []
+    current_user.charities.each do |charity_found|
+      @new_tasks << Task.current.where(charity_id: charity_found.id).includes([:charity, image_attachment: :blob])
+      @old_tasks << Task.previous.where(charity_id: charity_found.id).includes([:charity, image_attachment: :blob])
+    end
   end
 
+  # Show all people interested in a task
   def show
-    # Find people interested a particular task
+    # Logic similar to above
+    # For that task find all interests from tasks-intersts connection
+    # This may returns multiple interest rows
+    # Then for each row, find the user who has matching user_id
+    # Can use .find as only one user will have that user_id
     @found_people = []
-    @task.interests.each do |item|
-      interested_users = User.find(item.user_id)
-      @found_people << interested_users
+    @task.interests.each do |interest_found|
+      interested_users = User.find(interest_found.user_id)
+      @found_people << interested_users 
     end
   end
 
