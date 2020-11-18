802886904090562558cf696a80b6327723c86f2a
bronwyncarr <bronwynacarr@gmail.com>
2020-11-18 21:42:13 +1100

    user-task relation cut

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_060751) do
+ActiveRecord::Schema.define(version: 2020_11_18_100928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,9 +112,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_060751) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "charity_id", null: false
     t.text "summary"
-    t.bigint "user_id", null: false
     t.index ["charity_id"], name: "index_tasks_on_charity_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -142,6 +140,5 @@ ActiveRecord::Schema.define(version: 2020_11_16_060751) do
   add_foreign_key "required_skills", "skills"
   add_foreign_key "required_skills", "tasks"
   add_foreign_key "tasks", "charities"
-  add_foreign_key "tasks", "users"
   add_foreign_key "users", "roles"
 end
